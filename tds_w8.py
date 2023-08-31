0f65a07b2b1c8027d7c56341310cee8233e57c20
borishan <borishan.gh@gmail.com>
2023-08-31 20:06:31 +0530

    Update tds_w8.py

--- tds_w8.py
+++ tds_w8.py
@@ -1,3 +1,17 @@
-print("Enter three numbers")
-a, b, c = map(float, input().split(" "))
-print(max(a, b, c))
+import streamlit as st
+
+
+def main():
+    st.title("TDS W8 Assgn : Greatest of 3 numbers")
+    st.write("Borishan Ghosh \n 22f2000824")
+
+    st.write("Enter three Numbers : ")
+    a = st.number_input("a = ")
+    b = st.number_input("b = ")
+    c = st.number_input("c = ")
+
+    st.write("The largest number is : " + str(max(a, b, c)))
+
+
+if __name__ == "__main__":
+    main()
